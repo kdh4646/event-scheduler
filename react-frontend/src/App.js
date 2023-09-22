7ce91d8bb3887923c23607e341d989cb34307188
Derek Kweon <kdh4646@hanmail.net>
2023-09-21 21:15:36 -0700

    2023-09-21 update Events.js and other

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,3 @@
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
