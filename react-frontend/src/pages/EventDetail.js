347d281ec992cad75006314bb65901e8db527e62
Derek Kweon <kdh4646@hanmail.net>
2023-09-21 21:09:21 -0700

    2023-09-21 link other pages

--- react-frontend/src/pages/EventDetail.js
+++ react-frontend/src/pages/EventDetail.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
-  return <p>EventDetailPage</p>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 };
 
 export default EventDetailPage;
--- react-frontend/src/pages/Events.js
+++ react-frontend/src/pages/Events.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  { id: "e1", title: "Some event" },
+  { id: "e2", title: "Some event2" },
+];
+
 const EventsPage = () => {
-  return <p>EventsPage</p>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
