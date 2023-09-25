1eb65118d7c83dc394151ed10bcb27f404df4030
Derek Kweon <kdh4646@hanmail.net>
2023-09-25 16:12:03 -0700

    2023-09-25 using useFecther(), useEffct()

--- react-frontend/src/components/NewsletterSignup.js
+++ react-frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,35 @@
+import { useEffect } from "react";
+import { useFetcher } from "react-router-dom";
+
 import classes from "./NewsletterSignup.module.css";
 
 function NewsletterSignup() {
+  //not initialize a route transition
+  const fetcher = useFetcher();
+
+  //return by the action or loader
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    //state: idle, loading, submitting
+    if (state === "idle" && data && data.message) {
+      window.alert("Signup successful!");
+    }
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
