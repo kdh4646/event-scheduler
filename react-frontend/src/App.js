4fe4d595bdd62dc25d8e51f3179feea70351d034
Derek Kweon <kdh4646@hanmail.net>
2023-09-27 00:22:22 -0700

    2023-09-27 fixed CORS error, handle save button

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -10,7 +10,9 @@ import EditEventPage from "./pages/EditEvent";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
 
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -68,6 +70,7 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
       },
     ],
   },
